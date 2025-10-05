cbac9f2411fddbe4cbfae0539bd0a316f58f63a6
AnataAria <arisavinh2003@gmail.com>
2025-10-05 17:26:18 +0700

    feat: project init

---
+++ src/config/swagger.ts
@@ -0,0 +1,43 @@
+import type { Express } from "express";
+import swaggerJsdoc from "swagger-jsdoc";
+import swaggerUi from "swagger-ui-express";
+
+const options: swaggerJsdoc.Options = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Template API",
+      version: "1.0.0",
+      description: "API documentation for Template project",
+    },
+    servers: [
+      {
+        url: "http://localhost:3000",
+      },
+    ],
+    components: {
+      securitySchemes: {
+        bearerAuth: {
+          type: "http",
+          scheme: "bearer",
+          bearerFormat: "JWT",
+          description: "Enter JWT token",
+        },
+      },
+    },
+    security: [
+      {
+        bearerAuth: [],
+      },
+    ],
+  },
+  apis: ["./src/module/**/*.routes.ts"],
+};
+
+const swaggerSpec = swaggerJsdoc(options);
+
+export function swaggerDocs(app: Express, port: number) {
+  app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
+  console.log(`📄 Swagger running at http://localhost:${port}/docs`);
+}
